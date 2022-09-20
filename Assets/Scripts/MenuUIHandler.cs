b839925e617f39d5ca58a23713a08711c5c8a939
lakakarot <ibrahim@khan.is>
2022-09-20 16:26:48 -0400

    Menu logic
    Created menu handler and main manager scripts, tweaks on menu scenes

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HighScores()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void SaveSettings()
+    {
+
+    }
+
+    public void LoadSettings()
+    {
+
+    }
+}
